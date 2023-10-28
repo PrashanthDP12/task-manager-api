fc3f3821f1837abc0bfcd14b71f268d0091eff1b
Prashanth DP <1296prashanth@gmail.com>
2023-10-28 10:57:16 +0530

    Adding Middleware for GetProfile route to test JWT

--- src/index.js
+++ src/index.js
@@ -131,18 +131,25 @@
 //   console.log("Server is up on port " + port);
 // });
 
-const express = require('express')
-require('./db/mongoose')
-const userRouter = require('./routers/user')
-const taskRouter = require('./routers/task')
+const express = require("express");
+require("./db/mongoose");
+const userRouter = require("./routers/user");
+const taskRouter = require("./routers/task");
 
-const app = express()
-const port = process.env.PORT || 3000
+const app = express();
+const port = process.env.PORT || 3000;
 
-app.use(express.json())
-app.use(userRouter)
-app.use(taskRouter)
+// app.use((req, res, next) => {
+//   res.status(503).send("Sorry folks, Maintenance break, Please try again")
+//   console.log(req.method, req.path);
+//   next()
+// });
+
+
+app.use(express.json());
+app.use(userRouter);
+app.use(taskRouter);
 
 app.listen(port, () => {
-    console.log('Server is up on port ' + port)
-})
+  console.log("Server is up on port " + port);
+});
