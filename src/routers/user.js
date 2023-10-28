0e4c86905dbd74c2f762e11552b43209f134394e
Prashanth DP <1296prashanth@gmail.com>
2023-10-28 12:34:51 +0530

    Authenticating User related endpoints

--- src/routers/user.js
+++ src/routers/user.js
@@ -55,23 +55,23 @@ router.get("/users/me", auth, async (req, res) => {
   res.send(req.user);
 });
 
-router.get("/users/:id", async (req, res) => {
-  const _id = req.params.id;
+// router.get("/users/:id", async (req, res) => {
+//   const _id = req.params.id;
 
-  try {
-    const user = await User.findById(_id);
+//   try {
+//     const user = await User.findById(_id);
 
-    if (!user) {
-      return res.status(404).send();
-    }
+//     if (!user) {
+//       return res.status(404).send();
+//     }
 
-    res.send(user);
-  } catch (e) {
-    res.status(500).send();
-  }
-});
+//     res.send(user);
+//   } catch (e) {
+//     res.status(500).send();
+//   }
+// });
 
-router.patch("/users/:id", async (req, res) => {
+router.patch("/users/me", auth, async (req, res) => {
   const updates = Object.keys(req.body);
   const allowedUpdates = ["name", "email", "password", "age"];
   const isValidOperation = updates.every((update) =>
@@ -83,30 +83,24 @@ router.patch("/users/:id", async (req, res) => {
   }
 
   try {
-    const user = await User.findById(req.params.id);
-
+    // const user = await User.findById(req.params.id);
+    const user = req.user;
     updates.forEach((update) => (user[update] = req.body[update]));
     await user.save();
-
-    if (!user) {
-      return res.status(404).send();
-    }
-
     res.send(user);
   } catch (e) {
     res.status(400).send(e);
   }
 });
 
-router.delete("/users/:id", async (req, res) => {
+router.delete("/users/me", auth, async (req, res) => {
   try {
-    const user = await User.findByIdAndDelete(req.params.id);
-
-    if (!user) {
-      return res.status(404).send();
-    }
-
-    res.send(user);
+    // // req.user._id is set using auth, hence this will always hold id value.
+    // const user = await User.findByIdAndDelete(req.user._id);
+    // alternate way of using mongoose method
+    // await req.user.remove(); -> remove is deprecated
+    await req.user.deleteOne();
+    res.send(req.user);
   } catch (e) {
     res.status(500).send();
   }
